10c82f0e5275cf1e7ea8c92af3bfd3a2ab6d951c
unknown <bdcpas19@gmail.com>
2026-03-27 13:37:51 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,11 +1,11 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Mark";
+let userAge = 21;
 
 // ...
 
-userAge = "31"
+userAge = 21
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
